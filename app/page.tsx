9643e405f6706f9f41a83d5a862942fb2956f69d
coral-life-co <suchada@coralesg.com>
2025-09-17 12:09:35 +0000

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import HomeHeroBlock from "@/components/blocks/home-hero-block";
 import ChallengeBlock from "@/components/blocks/challenge-block";
 import TechnologyBlock from "@/components/blocks/technology-block";
 import SolutionsBlock from "@/components/blocks/solutions-block";
-import CTABlock from "@/components/blocks/cta-block";
+import { CTABlock } from "@/components/blocks/cta-block";
 
 
 export default function Home() {
